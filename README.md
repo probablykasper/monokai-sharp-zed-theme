6a213546d9c0f4d836a20d80eab8c929a99f6ef7
Kasper <kasperkh.kh@gmail.com>
2025-12-03 01:55:26 +0100

    Create theme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Monokai Sharp
+
+Zed version of Monokai Sharp theme, originally created by [0b5vr](https://github.com/0b5vr): https://github.com/0b5vr/vscode-monokai-sharp
+
+![Screenshot](./Screenshot.png)
---
+++ screenshot-file.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { getMonthLength, cloneDate, type CalendarDay } from './date-utils.js'
+
+	const getfirst = (...args: unknown[]) => {
+		return args[0]
+	}
+
+	function selectDay(calendarDay: CalendarDay) {
+		if (getMonthLength(2025, calendarDay.month) > 31) {
+			throw new Error('there is blood everywhere')
+		}
+		value.setFullYear(0)
+		value.setMonth(0)
+		value.setDate(1)
+		value.setFullYear(calendarDay.year)
+		value.setMonth(calendarDay.month)
+		value.setDate(calendarDay.number)
+		getfirst('select', cloneDate(value))
+	}
+
+	// Date value. It's `null` if no date is selected
+	export let value: Date = new Date()
+</script>
+
+<div class="date-time-picker" on:focusout>
+	<button
+		type="button"
+		aria-label="Previous month"
+		class="page-button"
+		on:click={() => selectDay({} as CalendarDay)}
+		class:selected={value.getFullYear() === 2025}
+	>
+		Text
+	</button>
+</div>
+
+<style lang="css">
+	.page-button {
+		display: inline-block;
+	}
+</style>
